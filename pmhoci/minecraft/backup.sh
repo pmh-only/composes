07878ee9728e02d4759de4fe1343ae5a6b1a8051
Minhyeok Park <pmh_only@pmh.codes>
2024-01-13 05:22:20 +0000

    refactor(mc): move minecraft instance to `pmhoci` server

--- pmhoci/minecraft/backup.sh
+++ pmhoci/minecraft/backup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 DATE=`date +%Y%m%d`
-FILE_COUNT=`ls /mnt/minecraft-backups -l | wc -l`
+FILE_COUNT=`ls /home/pmh/minecraft-backups -l | wc -l`
 
 cd /home/pmh/composes/$1/minecraft
 
 docker compose stop
 
 tar czf \
-  /mnt/minecraft-backups/$DATE.tar.gz \
+  /home/pmh/minecraft-backups/$DATE.tar.gz \
   ./data ./volumes/lobby/world \
   ./volumes/creative/void_1 ./volumes/creative/world_old \
   ./volumes/survival/world ./volumes/survival/world_nether ./volumes/survival/world_the_end
 
-echo $DATE > /mnt/minecraft-backups/LAST_DATE
+echo $DATE > /home/pmh/minecraft-backups/LAST_DATE
 
 docker compose up -d
 
 if [ $FILE_COUNT -gt 9 ]; then
-  rm /mnt/minecraft-backups/$(ls -rt /mnt/minecraft-backups | head -n 1)
+  rm /home/pmh/minecraft-backups/$(ls -rt /home/pmh/minecraft-backups | head -n 1)
 fi
